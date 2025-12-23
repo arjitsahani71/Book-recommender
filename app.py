9352a03c5970132d1c3b864108eb929476525766
Jay Sahani <saswatisahani0@gmail.com>
2025-12-24 01:23:43 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_score = pickle.load(open('similarity_score.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        book_name=list(popular_df['Book-Title'].values),
+        author=list(popular_df['Book-Author'].values),
+        image=list(popular_df['Image-URL-M'].values),
+        votes=list(popular_df['num_rating'].values),
+        rating=list(popular_df['avg_rating'].values)
+    )
+
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+
+@app.route('/recommend_books', methods=['POST'])
+def recommend_books():
+    user_input = request.form.get('user_input')
+
+    # If book not found
+    if user_input not in pt.index:
+        return render_template(
+            'recommend.html',
+            data=[],
+            error="Book not found. Please try another title."
+        )
+
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(
+        list(enumerate(similarity_score[index])),
+        key=lambda x: x[1],
+        reverse=True
+    )[1:5]
+
+    data = []
+
+    for i in similar_items:
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+
+        item = [
+            temp_df.drop_duplicates('Book-Title')['Book-Title'].values[0],
+            temp_df.drop_duplicates('Book-Title')['Book-Author'].values[0],
+            temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values[0]
+        ]
+
+        data.append(item)
+
+    return render_template('recommend.html', data=data)
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
